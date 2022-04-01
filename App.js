a4edd8d88dd75eef3f4ce59b7938037b0beca7a2
hamnaveed <addon.mobile7@gmail.com>
2022-04-01 16:54:44 +0500

    First Commit

--- App.js
+++ App.js
@@ -1,27 +1,107 @@
-import React from 'react';
+import React, { useEffect, useState,useCallback } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { GiftedChat } from 'react-native-gifted-chat'
 
+import {
+  View,
+  Text,
+  YellowBox,
+  StatusBar,
+  Button,
+  TextInput,
+  StyleSheet
+} from "react-native";
+//@ refresh reset
 import Tabs from "./navigation/tabs";
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
+const firebaseConfig = {
+  apiKey: "AIzaSyCZNBDU83h3eVF3Jh9_2_ocDxX0u2ah6Y0",
+  authDomain: "astrochat-32089.firebaseapp.com",
+  projectId: "astrochat-32089",
+  storageBucket: "astrochat-32089.appspot.com",
+  messagingSenderId: "628005365041",
+  appId: "1:628005365041:web:559773cacc92dd323ffc10",
+};
+let app;
+app = firebase.initializeApp(firebaseConfig);
 
-const Stack = createStackNavigator();
+const App = () => { 
+    const [user, setUser] = useState(null)
+    const [name, setName] = useState('')
+    const [messages, setMessages] = useState([])
+    
+const db = firebase.firestore()
+const chatsRef = db.collection('chats')
+  useEffect(() => {
+    readUser();
+    const unsubscribe = chatsRef.onSnapshot((querySnapshot) => {
+        const messagesFirestore = querySnapshot
+            .docChanges()
+            .filter(({ type }) => type === 'added')
+            .map(({ doc }) => {
+                const message = doc.data() 
+                return { ...message, createdAt: message.createdAt.toDate() }
+            })
+            .sort((a, b) => b.createdAt.getTime() - a.createdAt.getTime())
+        appendMessages(messagesFirestore)
+    })
+    return () => unsubscribe()
+  }, []);
+  const appendMessages = useCallback(
+    (messages) => {
+        setMessages((previousMessages) => GiftedChat.append(previousMessages, messages))
+    },
+    [messages]
+)
 
-const App = () => {
-    return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    headerShown: false
-                }}
-                initialRouteName={'MainLayout'}
-            >
-                <Stack.Screen
-                    name="MainLayout"
-                    component={Tabs}
-                />
-            </Stack.Navigator>
-        </NavigationContainer>
-    )
+  async function readUser() {
+    const user = await AsyncStorage.getItem('user')
+    if (user) {
+        setUser(JSON.parse(user))
+    }
+}
+async function handlePress() {
+    const _id = Math.random().toString(36).substring(7)
+    const user = { _id, name }
+    await AsyncStorage.setItem('user', JSON.stringify(user))
+    setUser(user)
+}
+async function handleSend(messages) {
+    const writes = messages.map((m) => chatsRef.add(m))
+    await Promise.all(writes)
 }
 
+if (!user) {
+    return (
+        <View style={styles.container}>
+            <TextInput style={styles.input} placeholder="Enter your name" value={name} onChangeText={setName} />
+            <Button onPress={handlePress} title="Enter the chat" />
+        </View>
+    )
+}
+  return (
+    <GiftedChat messages={messages} user={user} onSend={handleSend} />
+  );
+};
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 30,
+    },
+    input: {
+        height: 50,
+        width: '100%',
+        borderWidth: 1,
+        padding: 15,
+        marginBottom: 20,
+        borderColor: 'gray',
+    },
+})
 export default App;
